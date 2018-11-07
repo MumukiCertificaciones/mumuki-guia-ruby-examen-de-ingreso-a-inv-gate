5c6bfcea72ed319daef6ff9bd87f088d88292a35
agus@mumuki.org <agus@mumuki.org>
2018-11-07 14:39:18 -0500

    Mumuki Export on 2018-11-07 14:39:18 -0500

--- 00001_Nuevo ejercicio/test.rb
+++ 00001_Nuevo ejercicio/test.rb
@@ -1,6 +1,10 @@
-describe 'Descripción del grupo' do
-  it 'Descripción del ejemplo' do
+describe 'tests' do
+  it 'test1' do
     File.open("archivo.txt", ["w"]) {|f| f.write("Extranjero Rock Genero\nJazz Genero\nModerno Nacional Rock Género\nViejito Nacional Rock Género") }
-    expect(procesar_archivo("archivo.txt")).to eq true
+    expect(procesar_archivo("archivo.txt")).to eq "a"
+  end
+  it 'test2' do
+    File.open("archivo.txt", ["w"]) {|f| f.write("Genero Rock\nGenero Jazz\nRock Nacional\nRock Extranjero\nNacional Viejito\nNacional Moderno") }
+    expect(procesar_archivo("archivo.txt")).to eq "a"
   end
 end
